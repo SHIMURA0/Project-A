98debe72a01a17eabfcd2eba2e636027f51704a4
Honglie Zhang <hongliez27@gmail.com>
2024-10-09 17:06:23 +0800

    Initial Commit

---
+++ Pet/PetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetApp.swift
+//  Pet
+//
+//  Created by Shimura on 2024/10/9.
+//
+
+import SwiftUI
+
+@main
+struct PetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetTests/PetTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetTests.swift
+//  PetTests
+//
+//  Created by Shimura on 2024/10/9.
+//
+
+import Testing
+@testable import Pet
+
+struct PetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
